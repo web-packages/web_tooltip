87deae5b1cd18d5c5b925468c67cc7f8691b9e7a
MTtankkeo <ttankkeo112@gamil.com>
2024-02-16 05:20:14 +0900

    commit for develop

---
+++ js/controller.js
@@ -0,0 +1,13 @@
+
+export const Alignment = {
+    AUTO:        "auto",
+    CENTER:      "center",
+    TOP:         "top",
+    TOPLEFT:     "top_left",
+    TOPRIGHT:    "top_right",
+    BOTTOM:      "bottom",
+    BOTTOMLEFT:  "bottom_left",
+    BOTTOMRIGHT: "bottom_right",
+    LEFT:        "left",
+    RIGHT:       "right",
+}
--- js/element.js
+++ js/element.js
@@ -1,8 +1,56 @@
+import { Alignment } from "./controller.js";
 
-class Tooltip extends HTMLElement {
+class TooltipElement extends HTMLElement {
+    /**
+     * @param {string} key
+     * @param {HTMLElement} scope
+     */
+    getPropertyValue(key, scope = this) {
+        return getComputedStyle(scope).getPropertyValue(key);
+    }
+
+    /**
+     * @param {number} delay - time
+     */
+    handlePointerEvent(delay) {
+        this.onpointerleave = () => clearTimeout(this.delayTimer);
+
+        this.delayTimer = setTimeout(() => {
+            console.log("hello world");
+        }, delay);
+    }
+
     connectedCallback() {
-        
+        const text = this.getAttribute("text");
+        if (text == null) {
+            throw new Error("The required property [text] is undefined.");
+        }
+
+        const align = this.getAttribute("alignment") || "auto";
+        if (align != Alignment.AUTO
+         && align != Alignment.CENTER
+         && align != Alignment.TOP
+         && align != Alignment.TOPLEFT
+         && align != Alignment.TOPRIGHT
+         && align != Alignment.BOTTOM
+         && align != Alignment.BOTTOMLEFT
+         && align != Alignment.BOTTOMRIGHT
+         && align != Alignment.LEFT
+         && align != Alignment.RIGHT) {
+            throw new Error("The automatic property [alignment] is not correctly defined. [auto, top, bottom, left, right]");
+        }
+
+        const delay = this.getPropertyValue("--tooltip-delay") || "0.1s";
+        const delayValue = delay.endsWith("ms")
+            ? Number.parseFloat(delay)
+            : Number.parseFloat(delay) * 1000;
+
+        if (isNaN(delayValue)) {
+            throw new Error("The property [--tooltip-delay] is must be time unit(ms, s)");
+        }
+
+        this.onpointerenter = () => this.handlePointerEvent(delayValue);
     }
 }
 
-customElements.define("tool-tip", Tooltip);
+customElements.define("tool-tip", TooltipElement);
