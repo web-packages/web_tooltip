8808742a39ed89f792b84863d2ecf583a26a83d7
MTtankkeo <ttankkeo112@gamil.com>
2024-02-16 07:11:50 +0900

    commit for develop

--- js/controller.js
+++ js/controller.js
@@ -1,5 +1,5 @@
 
-export const Alignment = {
+export const TooltipAlignment = {
     AUTO:        "auto",
     CENTER:      "center",
     TOP:         "top",
@@ -10,4 +10,58 @@ export const Alignment = {
     BOTTOMRIGHT: "bottom_right",
     LEFT:        "left",
     RIGHT:       "right",
+}
+
+/**
+ * @param {string} value
+ * @returns {boolean}
+ */
+export function isTooltipAlignment(value) {
+    if (value != TooltipAlignment.AUTO
+     && value != TooltipAlignment.CENTER
+     && value != TooltipAlignment.TOP
+     && value != TooltipAlignment.TOPLEFT
+     && value != TooltipAlignment.TOPRIGHT
+     && value != TooltipAlignment.BOTTOM
+     && value != TooltipAlignment.BOTTOMLEFT
+     && value != TooltipAlignment.BOTTOMRIGHT
+     && value != TooltipAlignment.LEFT
+     && value != TooltipAlignment.RIGHT) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+export class TooltipController {
+    /** @param {HTMLElement} element  */
+    set current(element) {
+        this._current = element;
+    }
+
+    /** @returns {HTMLElement} */
+    get current() { return this._current};
+
+    /**
+     * @param {HTMLElement} element 
+     * @param {string} alignment
+     * @param {HTMLElement} parent
+    */
+    static show(
+        element,
+        alignment = "auto",
+        parent = document.getElementsByTagName("body")[0]
+    ) {
+        if (element == null) {
+            throw new Error("the required parameter [element] is undefined.");
+        }
+        if (!isTooltipAlignment(alignment)) {
+            throw new Error("the required parameter [alignment] is not correctly defined. (refer to [TooltipAlignment] for details)");
+        }
+        if (parent == null) {
+            throw new Error("The required parameter [parent] is undefined.");
+        }
+
+        this.current = element;
+    }
 }
--- js/element.js
+++ js/element.js
@@ -1,4 +1,4 @@
-import { Alignment } from "./controller.js";
+import { TooltipController, isTooltipAlignment } from "./controller.js";
 
 class TooltipElement extends HTMLElement {
     /**
@@ -10,14 +10,25 @@ class TooltipElement extends HTMLElement {
     }
 
     /**
+     * @param {string} text
+     * @param {string} alignment
      * @param {number} delay - time
      */
-    handlePointerEvent(delay) {
+    handlePointerEvent(text, alignment, delay) {
         this.onpointerleave = () => clearTimeout(this.delayTimer);
 
         this.delayTimer = setTimeout(() => {
-            console.log("hello world");
+            TooltipController.show(this.createTooltipElement(text), alignment);
         }, delay);
+    }
+
+    /** @param {string} text  */
+    createTooltipElement(text) {
+        const element = document.createElement("p");
+        element.textContent = text;
+        element.className = "tooltip";
+
+        return element;
     }
 
     connectedCallback() {
@@ -26,30 +37,21 @@ class TooltipElement extends HTMLElement {
             throw new Error("The required property [text] is undefined.");
         }
         
-        const align = this.getAttribute("alignment") || "auto";
-        if (align != Alignment.AUTO
-         && align != Alignment.CENTER
-         && align != Alignment.TOP
-         && align != Alignment.TOPLEFT
-         && align != Alignment.TOPRIGHT
-         && align != Alignment.BOTTOM
-         && align != Alignment.BOTTOMLEFT
-         && align != Alignment.BOTTOMRIGHT
-         && align != Alignment.LEFT
-         && align != Alignment.RIGHT) {
-            throw new Error("The automatic property [alignment] is not correctly defined. [auto, top, bottom, left, right]");
+        const alignment = this.getAttribute("alignment") || "auto";
+        if (!isTooltipAlignment(alignment)) {
+            throw new Error("The automatic property [alignment] is not correctly defined. (refer to [TooltipAlignment] for details)");
         }
 
         const delay = this.getPropertyValue("--tooltip-delay") || "0.1s";
         const delayValue = delay.endsWith("ms")
             ? Number.parseFloat(delay)
             : Number.parseFloat(delay) * 1000;
-
+        
         if (isNaN(delayValue)) {
             throw new Error("The property [--tooltip-delay] is must be time unit(ms, s)");
         }
 
-        this.onpointerenter = () => this.handlePointerEvent(delayValue);
+        this.onpointerenter = () => this.handlePointerEvent(text, alignment, delayValue);
     }
 }
 
